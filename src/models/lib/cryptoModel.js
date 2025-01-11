201514177084af69b3e91ea19f83341d25e6ec8a
KARTIK5799 <thekartiksapkal@gmail.com>
2025-01-11 15:13:38 +0530

    Updated: After the addition of the deviation route, an array has been included to store the history of prices

--- src/models/lib/cryptoModel.js
+++ src/models/lib/cryptoModel.js
@@ -4,8 +4,10 @@ const cryptoStatsSchema = new mongoose.Schema({
   coin: { type: String, required: true, enum: ['bitcoin', 'matic-network', 'ethereum'] },
   price: { type: Number, required: true },
   marketCap: { type: Number, required: true },
+  priceHistory: { type: [Number], default: []},
   change24h: { type: Number, required: true },
   timestamp: { type: Date, default: Date.now }
 });
+
 
 export const CryptoStats = mongoose.model('CryptoStats', cryptoStatsSchema);
